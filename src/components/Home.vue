cc0807eba71897bb6dd1d1eec8099ca3457faf91
Dušan Simić <dusan.simic1810@gmail.com>
2017-11-16 08:50:00 +0100

    Izmena da se koristi jest umesto karma+mocha

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,18 @@
 <template>
 		<div class="Home">
 			<img src="../assets/profile.jpg" alt="Dusan Simic"></img>
-			<p id="name"><b>{{ myName }}</b></p>
+			<p id="name"><strong>Dusan Simic</strong></p>
 		</div>
 </template>
 
 <script>
 export default {
-	name: 'Home',
-	data () {
-		return {
-			myName: ''
-		}
-	},
-	created: function() {
-		fetch('http://localhost:3000/api/myname').then(res => res.json()).then(data => {
-			this.myName = data.name.toString() + ' ' + data.surname.toString();
-		}).catch(err => {
-			if (err) {
-				console.error(err);
-			}
-		});
-	}
+	name: 'Home'
 }
 </script>
 
 <style>
+#name {
+	font-size: 24px;
+}
 </style>
